7dc1e6e414bc7064da955cad5582078d65d2cd04
Andrew Cho and Sean Peerenboom <seanmp42@gmail.com and cho-andrew@hotmail.com>
2016-02-25 11:38:04 -0800

    get ability to clear list of all cuisines

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -16,14 +16,15 @@ namespace cuisineRestaurants
       Get ["/cuisine/new"] = _ =>{
         return View ["cuisine_info.cshtml"];
       };
-      // Get["/testing"]=_=>{
-      //   return View ["index.cshtml"];
-      // };
       Post["/"] = _ => {
         Cuisines newCuisines = new Cuisines(Request.Form["cuisine-name"]);
         newCuisines.Save();
         List<Cuisines> allCuisines = Cuisines.GetAll();
         return View["index.cshtml", allCuisines];
+      };
+      Get["/cuisine/ClearAll"] = _ => {
+        Cuisines.DeleteAll();
+        return View ["cuisineClearAll.cshtml"];
       };
     }
   }
